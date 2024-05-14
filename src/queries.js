9695285b503659a399b1d940221d5d6332f5c0c9
utkuarslan5 <utku.arslan@hotmail.com>
2024-05-14 09:42:36 +0200

    back to simplicty WIP

--- src/queries.js
+++ src/queries.js
@@ -1,335 +1,61 @@
 import { HttpError } from "wasp/server";
 
-export const getUser = async ({ id }, context) => {
-  if (!context.user) {
-    throw new HttpError(401);
+export const getLoops = async (_args, context) => {
+  try {
+    return context.entities.Loop.findMany({
+      orderBy: {
+        id: "desc",
+      },
+    });
+  } catch (error) {
+    throw new HttpError(500, error.message);
   }
+};
 
+export const getActiveLoops = async (_args, context) => {
+  try {
+    return context.entities.Loop.findMany({
+      where: {
+        isActive: true,
+      },
+      orderBy: {
+        id: "desc",
+      },
+    });
+  } catch (error) {
+    throw new HttpError(500, error.message);
+  }
+};
+
+export const getUsernameFromLoopID = async ({ id }, context) => {
   try {
     const user = await context.entities.User.findUnique({
-      where: { id },
+      where: {
+        id,
+      },
       select: {
-        id: true,
         username: true,
-        profileImage: true,
-        createdLoops: {
-          select: {
-            id: true,
-            name: true,
-            projectType: true,
-            numIterations: true,
-            frequency: true,
-            isActive: true,
-            createdBy: {
-              select: {
-                id: true,
-              },
-            },
-            watchers: {
-              select: {
-                id: true,
-              },
-            },
-            participants: {
-              select: {
-                id: true,
-                username: true,
-              },
-            },
-            iterations: true,
-          },
-        },
-        watchedLoops: {
-          select: {
-            id: true,
-            name: true,
-            projectType: true,
-            numIterations: true,
-            frequency: true,
-            isActive: true,
-            createdBy: {
-              select: {
-                id: true,
-              },
-            },
-            participants: {
-              select: {
-                id: true,
-                username: true,
-              },
-            },
-            iterations: true,
-          },
-        },
-        participatedLoops: {
-          select: {
-            id: true,
-            name: true,
-            projectType: true,
-            numIterations: true,
-            frequency: true,
-            isActive: true,
-            createdBy: {
-              select: {
-                id: true,
-              },
-            },
-            watchers: {
-              select: {
-                id: true,
-              },
-            },
-            participants: {
-              select: {
-                id: true,
-                username: true,
-              },
-            },
-            iterations: true,
-          },
-        },
       },
     });
 
-    if (!user) throw new HttpError(404, "No user with id " + id);
-
-    return user;
+    return user?.username ?? null;
   } catch (error) {
     throw new HttpError(500, error.message);
   }
 };
 
-export const getUserByUsername = async ({ username }, context) => {
+export const getIterations = async ({ loopId }, context) => {
   try {
-    const user = await context.entities.User.findUnique({
-      where: { username },
-      select: {
-        id: true,
-        username: true,
-        profileImage: true,
-        createdLoops: {
-          select: {
-            id: true,
-            name: true,
-            projectType: true,
-            numIterations: true,
-            frequency: true,
-            isActive: true,
-            watchers: {
-              select: {
-                id: true,
-              },
-            },
-            participants: {
-              select: {
-                id: true,
-              },
-            },
-            iterations: true,
-          },
-        },
-        watchedLoops: {
-          select: {
-            id: true,
-            name: true,
-            projectType: true,
-            numIterations: true,
-            frequency: true,
-            isActive: true,
-            createdBy: {
-              select: {
-                id: true,
-              },
-            },
-            participants: {
-              select: {
-                id: true,
-              },
-            },
-            iterations: true,
-          },
-        },
-        participatedLoops: {
-          select: {
-            id: true,
-            name: true,
-            projectType: true,
-            numIterations: true,
-            frequency: true,
-            isActive: true,
-            createdBy: {
-              select: {
-                id: true,
-                username: true,
-              },
-            },
-            watchers: {
-              select: {
-                id: true,
-              },
-            },
-            participants: {
-              select: {
-                id: true,
-              },
-            },
-            iterations: true,
-          },
+    return context.entities.Iteration.findMany({
+      where: {
+        loop: {
+          id: loopId,
         },
       },
-    });
-
-    if (!user) {
-      throw new HttpError(404, "No user with username " + username);
-    }
-    return user;
-  } catch (error) {
-    console.error(`Error getting user by username: ${error.message}`);
-    throw new HttpError(500, error.message);
-  }
-};
-
-export const getLoops = async (args, context) => {
-  try {
-    const loops = await context.entities.Loop.findMany();
-
-    return loops || [];
-  } catch (error) {
-    throw new HttpError(500, error.message);
-  }
-};
-
-export const getActiveLoops = async (args, context) => {
-  try {
-    const activeLoops = await context.entities.Loop.findMany({
-      where: { isActive: true },
-      include: {
-        iterations: true,
-        createdBy: true,
-        watchers: {
-          select: {
-            id: true,
-            username: true,
-          },
-        },
-        participants: {
-          select: {
-            id: true,
-            username: true,
-          },
-        },
+      orderBy: {
+        endTime: "desc",
       },
     });
-
-    return activeLoops;
-  } catch (error) {
-    throw new HttpError(500, error.message);b
-  }
-};
-
-export const getLoopById = async ({ id }, context) => {
-  try {
-    const loop = await context.entities.Loop.findUnique({
-      where: { id },
-      include: {
-        iterations: true,
-        createdBy: true,
-        watchers: {
-          select: {
-            id: true,
-          },
-        },
-        participants: {
-          select: {
-            id: true,
-          },
-        },
-      },
-    });
-
-    if (!loop) throw new HttpError(404, "No loop with id " + id);
-
-    return loop;
-  } catch (error) {
-    throw new HttpError(500, error.message);
-  }
-};
-
-export const getLoopParticipants = async ({ loopId }, context) => {
-  try {
-    const participants = await context.entities.User.findMany({
-      where: { participatedLoops: { some: { id: loopId } } },
-    });
-
-    return participants;
-  } catch (error) {
-    throw new HttpError(500, error.message);
-  }
-};
-
-export const getLoopWatchers = async ({ loopId }, context) => {
-  try {
-    const watchers = await context.entities.User.findMany({
-      where: { watchedLoops: { some: { id: loopId } } },
-    });
-
-    return watchers;
-  } catch (error) {
-    throw new HttpError(500, error.message);
-  }
-};
-
-export const getIterationsByLoopId = async ({ loopId }, context) => {
-  try {
-    console.log("Getting iterations by loopId:", loopId);
-    const iterations = await context.entities.Iteration.findMany({
-      where: { loopId },
-      include: {
-        checkins: {
-          include: {
-            user: {
-              select: {
-                id: true,
-                username: true,
-                profileImage: true,
-                bio: true,
-                createdLoops: true,
-                watchedLoops: true,
-                participatedLoops: true,
-              },
-            },
-          },
-        },
-      },
-    });
-
-    if (!iterations || iterations.length === 0) {
-      throw new HttpError(404, "No iterations found for the given loop ID");
-    }
-
-    console.log("Iterations found:", iterations.length);
-    return iterations;
-  } catch (error) {
-    console.error("Error getting iterations:", error);
-    throw new HttpError(500, error.message);
-  }
-};
-
-
-export const getCheckinsByIterationId = async ({ iterationId }, context) => {
-  try {
-    const checkins = await context.entities.Checkin.findMany({
-      where: { iterationId },
-      include: {
-        user: true,
-      },
-    });
-
-    if (!checkins || checkins.length === 0) {
-      throw new HttpError(404, "No checkins found for the given iteration ID");
-    }
-
-    return checkins;
   } catch (error) {
     throw new HttpError(500, error.message);
   }
---
+++ src/scripts.js
@@ -0,0 +1,132 @@
+import { faker } from "@faker-js/faker";
+
+// in a terminal window run `wasp db seed` to seed your dev database with mock user data
+export function createRandomUser() {
+  const firstName = faker.person.firstName();
+  const lastName = faker.person.lastName();
+  const user = {
+    email: faker.internet.email({
+      firstName,
+      lastName,
+    }),
+    username: faker.internet.userName({
+      firstName,
+      lastName,
+    }),
+    createdAt: faker.date.between({
+      from: new Date("2023-01-01"),
+      to: new Date(),
+    }),
+    lastActiveTimestamp: faker.date.recent(),
+    isAdmin: false,
+    stripeId: `cus_${faker.string.uuid()}`,
+    sendEmail: false,
+    subscriptionStatus: faker.helpers.arrayElement([
+      "active",
+      "canceled",
+      "past_due",
+      "deleted",
+      null,
+    ]),
+    datePaid: faker.date.recent(),
+    credits: faker.number.int({ min: 0, max: 3 }),
+    checkoutSessionId: null,
+    subscriptionTier: faker.helpers.arrayElement(["hobby", "pro"]),
+  };
+  return user;
+}
+
+export function createRandomLoop() {
+  const name = faker.lorem.words(3);
+  const description = faker.lorem.words(10);
+  const accountabilityPartner = faker.internet.userName();
+  const isActive = faker.datatype.boolean();
+
+  const loop = {
+    name,
+    description,
+    accountabilityPartner,
+    isActive,
+  };
+
+  return loop;
+}
+
+export function createRandomIteration(loopId) {
+  const startTime = faker.date.recent();
+  const endTime = faker.date.future();
+  const checkin = faker.lorem.words(10);
+  const isComplete = faker.datatype.boolean();
+
+  const iteration = {
+    loopId,
+    startTime,
+    endTime,
+    checkin,
+    isComplete,
+  };
+
+  return iteration;
+}
+
+const USERS = faker.helpers.multiple(createRandomUser, {
+  count: 15,
+});
+
+export async function devSeedUsers(prismaClient) {
+  try {
+    await Promise.all(
+      USERS.map(async (user) => {
+        const createdUser = await prismaClient.user.create({
+          data: user,
+        });
+
+        // Create random number of loops for each user
+        const numLoops = faker.number.int({ min: 1, max: 5 });
+        for (let i = 0; i < numLoops; i++) {
+          const loop = createRandomLoop();
+          const createdLoop = await prismaClient.loop.create({
+            data: {
+              ...loop,
+              user: {
+                connect: { id: createdUser.id },
+              },
+            },
+          });
+
+          // Create random iterations for the loop
+          const numIterations = faker.number.int({ min: 1, max: 10 });
+          for (let j = 0; j < numIterations; j++) {
+            await prismaClient.iteration.create({
+              data: createRandomIteration(createdLoop.id),
+            });
+          }
+        }
+      })
+    );
+  } catch (error) {
+    console.error(error);
+  }
+}
+export async function deleteAll(prismaClient) {
+  await prismaClient.iteration.deleteMany({});
+  await prismaClient.loop.deleteMany({});
+  await prismaClient.user.deleteMany({});
+}
+
+export async function resetSequences(prismaClient) {
+  await prismaClient.$queryRaw`SELECT setval('"User_id_seq"', (SELECT MAX(id) FROM "User"));`;
+  await prismaClient.$queryRaw`SELECT setval('"Loop_id_seq"', (SELECT MAX(id) FROM "Loop"));`;
+  await prismaClient.$queryRaw`SELECT setval('"Iteration_id_seq"', (SELECT MAX(id) FROM "Iteration"));`;
+}
+
+export async function resetAll(prismaClient) {
+  try {
+    await deleteAll(prismaClient);
+    await resetSequences(prismaClient);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await prismaClient.$disconnect();
+  }
+}
